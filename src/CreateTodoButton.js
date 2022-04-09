ad0e5698115cc3a4d4b6de5ab30f6e01b3b4327b
Emanuel Schemberger <emanueldev24@gmail.com>
2022-04-09 15:47:37 -0300

    day 1

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+  const onClickButton = (msg) => {
+    alert(msg);
+  };
   return (
-    <button className="CreateTodoButton">+</button>
-  );
-}
+    <button className="CreateTodoButton" 
+    ionClick={() => onClickButton('Aqui deberá abrir el modal')}
+    >+</button>)  
+};
+
 
 export { CreateTodoButton };
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,7 @@ import './TodoCounter.css';
 
 function TodoCounter() {
   return (
-    <h2 className="TodoCounter">Has completado 2 de 3 TODOs</h2>
+    <h2 className="TodoCounter">Todo Counter</h2>
   );
 }
 
